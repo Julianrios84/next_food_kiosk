c71b436082dfe7f6c1415356ec84e94dcd3bac8b
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-22 13:24:50 -0500

    Agregabdi las imágenes  y Creando Sidebar basico

---
+++ kiosk-client/components/Sidebar.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+const Sidebar = () => {
+  return (
+    <>
+      <Image 
+        width={300}
+        height={100}
+        src="/assets/img/logo.svg"
+        alt="image logo"
+      />
+    </>
+  )
+}
+
+export default Sidebar
