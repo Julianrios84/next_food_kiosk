84da98ba8d5ecd0b05190a6d738dfeb3298e9dab
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-22 00:45:24 -0500

    Donde no se puede consultar la base de datos con Prisma

--- kiosk-client/pages/index.js
+++ kiosk-client/pages/index.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import { PrismaClient } from '@prisma/client'
 
 export default function Home({categories}) {
+
+  useEffect(() => {
+    const consultDb =  async () => {
+      const prisma = new PrismaClient();
+      const categories = await prisma.category.findMany() 
+      console.log("🚀 ~ file: index.js:10 ~ consult ~ categories", categories)
+    }
+    consultDb()
+  }, [])
+  
+
   return (
     <div>
       <h1 className=''>Next</h1>
