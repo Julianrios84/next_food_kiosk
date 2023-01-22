4ccc7c1986a90203cabb3fdb836dff11407afab3
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-22 00:39:48 -0500

    Consultando la base de datos con getServerSideProps

--- kiosk-client/pages/index.js
+++ kiosk-client/pages/index.js
@@ -1,9 +1,20 @@
+import { PrismaClient } from '@prisma/client'
 
-
-export default function Home() {
+export default function Home({categories}) {
   return (
     <div>
       <h1 className=''>Next</h1>
     </div>
   )
+}
+
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categories = await prisma.category.findMany()
+  return {
+    props: {
+      categories
+    }
+  }
 }
