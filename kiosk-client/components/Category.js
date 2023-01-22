aa3f2b19cc626dcfa05beb6debe5db4e18f2a9e1
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-22 14:57:36 -0500

    Detectando la categoria actual

--- kiosk-client/components/Category.js
+++ kiosk-client/components/Category.js
@@ -1,12 +1,17 @@
-import Image from "next/image"
+import Image from 'next/image';
+import useKiosk from '../hooks/useKiosk';
 
-const Category = ({category}) => {
-
-  const { name, icon, id } = category
+const Category = ({ category }) => {
+  const { categoryCurrent, handleClickCategory } = useKiosk();
+  const { name, icon, id } = category;
 
   return (
-    <div className="flex items-center gap-4 w-full border p-5 hover:bg-amber-400">
-      <Image 
+    <div
+      className={`${
+        categoryCurrent?.id === id ? 'bg-amber-400' : ''
+      }flex items-center gap-4 w-full border p-5 hover:bg-amber-400`}
+    >
+      <Image
         width={70}
         height={70}
         src={`/assets/img/icono_${icon}.svg`}
@@ -14,11 +19,15 @@ const Category = ({category}) => {
         className="mr-5"
       />
 
-      <button type="button" className="text-2xl font-bold hover:cursor-pointer">
+      <button
+        type="button"
+        className="text-2xl font-bold hover:cursor-pointer"
+        onClick={() => handleClickCategory(id)}
+      >
         {name}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- kiosk-client/context/KioskProvider.jsx
+++ kiosk-client/context/KioskProvider.jsx
@@ -4,17 +4,23 @@ const KioskContext = createContext();
 
 const KioskProvider = ({ children }) => {
   const [categories, setCategories] = useState([]);
+  const [categoryCurrent, setCategoryCurrent] = useState({})
 
   const getCategories = async () => {
     const { data } = await axios('api/categories');
     setCategories(data);
   };
 
+  const handleClickCategory = (id) => {
+    const category = categories.filter(item => item.id === id)
+    setCategoryCurrent(category[0])
+  }
+
   useEffect(() => {
     getCategories();
   }, []);
 
-  return <KioskContext.Provider value={{categories}}>{children}</KioskContext.Provider>;
+  return <KioskContext.Provider value={{categories, categoryCurrent, handleClickCategory}}>{children}</KioskContext.Provider>;
 };
 
 export { KioskProvider };
