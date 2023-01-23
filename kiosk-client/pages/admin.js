7feded6daa8e2dd34a5981e2e8670613f7ee7538
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-23 00:46:29 -0500

    Instalando SWR & Consultando el endpoint de ordenes con useSWR

--- kiosk-client/pages/admin.js
+++ kiosk-client/pages/admin.js
@@ -1,6 +1,12 @@
+import useSWR from 'swr'
+import axios from 'axios'
 import LayoutAdmin from "../layout/LayoytAdmin";
 
 export default function Admin() {
+
+  const fetcher = () => axios('/api/orders').then(data => data.data);
+  const { data, error, isLoading } = useSWR('/api/orders', fetcher)
+
   return (
     <LayoutAdmin page={'Admin'}>
       <h1 className="text-4xl font-black">Panel de Administración</h1>
