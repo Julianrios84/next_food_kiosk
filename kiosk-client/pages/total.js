d08b22f15d939ed0296908e24205a22dac48e5b8
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2023-01-22 21:48:31 -0500

    Evitar que el usuario pueda enviar el formulario si no esta completo el pedido

--- kiosk-client/pages/total.js
+++ kiosk-client/pages/total.js
@@ -1,31 +1,59 @@
-import Layout from '../layout/Layout'
+import { useCallback, useEffect } from 'react';
+import Layout from '../layout/Layout';
+import useKiosk from '../hooks/useKiosk';
 
 export default function Total() {
+  const { order } = useKiosk();
+
+  const checkOrder = useCallback(() => {
+    return order.length === 0;
+  }, [order]);
+
+  useEffect(() => {
+    checkOrder();
+  }, [order, checkOrder]);
+
+  const acceptOrder = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Layout page='Resumen'>
-    <h1 className='text-4xl font-black'>Total y Confirmar pedido</h1>
-    <p className='text-2xl my-10'>Confirma tu pedido a continuación</p>
+    <Layout page="Resumen">
+      <h1 className="text-4xl font-black">Total y Confirmar pedido</h1>
+      <p className="text-2xl my-10">Confirma tu pedido a continuación</p>
 
-
-
-    <form>
-      <div>
-        <label htmlFor="name" className='block capitalize text-slate-800 font-bold text-xl'>
-          Nombre
-        </label>
-        <input id='name' type="text" className='bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded-md' />
-      </div>
-      <div className='mt-10'>
-        <p className='text-2xl'>
-          Total a pagar {""} <span className='font-bold'></span>
-        </p>
-      </div>
-      <div className='mt-5'>
-        <input className='w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center'  value="Confirmar pedido" />
-      </div>
-    </form>
-
-
-  </Layout>
-  )
+      <form onSubmit={acceptOrder}>
+        <div>
+          <label
+            htmlFor="name"
+            className="block capitalize text-slate-800 font-bold text-xl"
+          >
+            Nombre
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded-md"
+          />
+        </div>
+        <div className="mt-10">
+          <p className="text-2xl">
+            Total a pagar {''} <span className="font-bold"></span>
+          </p>
+        </div>
+        <div className="mt-5">
+          <input
+            type="submit"
+            className={`${
+              checkOrder()
+                ? 'bg-indigo-100'
+                : 'bg-indigo-600 hover:bg-indigo-800'
+            } w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center`}
+            value="Confirmar pedido"
+            disabled={checkOrder()}
+          />
+        </div>
+      </form>
+    </Layout>
+  );
 }
